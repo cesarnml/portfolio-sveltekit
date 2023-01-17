0b6e8a5b4088efd923a4b9b29fda1dbac1851b7c
Cesar Mejia <cmejia@gmail.com>
2023-01-17 19:45:40 +0700

    add tailwind and daisyui, format all fiels with prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex justify-center items-center min-h-screen">
+	<button class="btn btn-primary">Hello world!</button>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
