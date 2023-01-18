c26e7713a69743144b28509dca37a2eb28edb85c
Cesar Mejia <cmejia@gmail.com>
2023-01-18 10:50:03 +0700

    fix playwright setup

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('page should have "Hello world" button', async ({ page }) => {
   await page.goto('http://localhost:4173/');
-  expect(page.getByText("Hello world")).toContainText('Hello world')
-  expect(page.getByText("Hello world")).toHaveClass('btn')
+  const target = page.getByText('Hello world');
+  await expect(target).toBeDefined()
 });
