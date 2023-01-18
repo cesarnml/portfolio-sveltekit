cb45b4f354c9204a67c95b8cb06b2884d6fb91ff
Cesar Mejia <cmejia@gmail.com>
2023-01-18 12:18:53 +0700

    remove playwright config timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,10 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173,
-		timeout: 120 * 1000,
+		port: 4173
 	},
 	testDir: 'e2e',
-	reporter: [['html', { open: 'never' }]],
+	reporter: [['html', { open: 'never' }]]
 }
 
 export default config
