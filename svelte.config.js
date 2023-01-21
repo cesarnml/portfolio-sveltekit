a84e180837138da1d196c18ff2d100399cc15c18
Cesar Mejia <cmejia@gmail.com>
2023-01-22 00:08:02 +0700

    fix preproces bug

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,6 @@ import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import { mdsvex } from 'mdsvex'
 import { resolve } from 'path'
-import preprocess from 'svelte-preprocess'
 import mdsvexConfig from './mdsvex.config.js'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,13 +10,7 @@ const config = {
 
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		}),
-		mdsvex(mdsvexConfig),
-		vitePreprocess()
-	],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
 		adapter: adapter(),
