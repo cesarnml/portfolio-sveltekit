6b8d6a29f262d305dc2d3b19ac80bcb723bc78a3
Cesar Mejia <cmejia@gmail.com>
2023-01-23 02:34:32 +0700

    downgrade testing library to fix ts bug

--- src/components/ReusableImage.spec.ts
+++ src/components/ReusableImage.spec.ts
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen } from '@testing-library/svelte'
+import { render, screen } from '@testing-library/svelte'
 import ReusableImage from './ReusableImage.svelte'
 
 describe('ReusableImage', () => {
