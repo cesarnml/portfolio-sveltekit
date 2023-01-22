71a250421b159cad6950eaa825845b403a267dd6
Cesar Mejia <cmejia@gmail.com>
2023-01-22 12:32:39 +0700

    minor

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('page should have "Hello world" button', async ({ page }) => {
 	await page.goto('http://localhost:4173/')
 	const target = page.getByText('Hello world')
-	await expect(target).toBeDefined()
+	expect(target).toBeDefined()
 })
 
 test('page have a link that navigates to a blog post', async ({ page }) => {
