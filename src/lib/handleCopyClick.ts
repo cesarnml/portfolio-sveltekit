1819104e93a348db106f8c6d2903120fd8864b28
Cesar Mejia <cmejia@gmail.com>
2023-01-24 18:01:07 +0700

    remove unused copy to clipboard logic

--- src/lib/handleCopyClick.ts
+++ src/lib/handleCopyClick.ts
@@ -1,9 +1,5 @@
-import { retrieveTextToCopy } from './retrieveTextToCopy'
-import { copyToClipboard } from './copyToClipboard'
 import { showCopyFeedbackAnimation } from './showCopyFeedbackAnimation'
 
 export const handleCopyClick = (evt: MouseEvent | TouchEvent) => {
-	// const textToCopy = retrieveTextToCopy(evt)
-	// copyToClipboard(textToCopy)
 	showCopyFeedbackAnimation(evt.target as HTMLButtonElement)
 }
