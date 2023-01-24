490ace1e8490ba2fcda0d031a0809b7d35adab68
Cesar Mejia <cmejia@gmail.com>
2023-01-24 18:33:55 +0700

    implement basic Footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="border-slate-600 bg-slate-900/75 py-8 text-sm text-center border-t">
+	<div>
+		<span>Copyright &copy; 2023.</span> <span>Made with ❤️ and ☕️ by Cesar Mejia</span>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
+	import Footer from '@components/Footer.svelte'
 	import Navbar from '@components/Navbar.svelte'
+	import Sidebar from '@components/Sidebar.svelte'
 	import { media } from '@stores/media'
 	import { sidebar } from '@stores/sidebar'
 	import '@styles/app.css'
-	import Sidebar from '@components/Sidebar.svelte'
+
 	// Reactively close sidebar if screen > media.sm and sidebar is open
 	$: if ($media.sm && $sidebar) {
 		sidebar.close()
 	}
 </script>
 
-<div class="bg-slate-800 text-slate-300 min-h-screen">
+<div class="bg-slate-800 text-slate-300 flex flex-col min-h-screen">
 	<Navbar />
 	{#if $sidebar}
 		<Sidebar />
 	{/if}
-	<div class="min-h-full py-4">
-		<slot />
+	<div class="flex flex-col flex-grow min-h-full pt-4">
+		<div class="flex-grow">
+			<slot />
+		</div>
+		<Footer />
 	</div>
 </div>
