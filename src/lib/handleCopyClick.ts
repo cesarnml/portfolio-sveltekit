781e1aea18aff0de434704ad65e784de5365080d
Cesar Mejia <cmejia@gmail.com>
2023-01-25 07:40:23 +0700

    restore commented out implementation (ssr bug)

--- src/lib/handleCopyClick.ts
+++ src/lib/handleCopyClick.ts
@@ -1,5 +1,9 @@
+import { retrieveTextToCopy } from './retrieveTextToCopy'
+import { copyToClipboard } from './copyToClipboard'
 import { showCopyFeedbackAnimation } from './showCopyFeedbackAnimation'
 
 export const handleCopyClick = (evt: MouseEvent | TouchEvent) => {
+	// const textToCopy = retrieveTextToCopy(evt)
+	// copyToClipboard(textToCopy)
 	showCopyFeedbackAnimation(evt.target as HTMLButtonElement)
 }
