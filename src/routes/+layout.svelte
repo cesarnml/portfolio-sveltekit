e874535ef129d1e853095a1024b8bf2f1baff6c9
Cesar Mejia <cmejia@gmail.com>
2023-01-28 15:15:31 +0700

    minor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -44,9 +44,9 @@
 		class="dark:prose-invert md:p-8 prose-a:no-underline hover:prose-a:text-secondary-800 dark:hover:prose-a:text-primary-500 prose-headings:mt-0 container relative max-w-screen-md p-4 mx-auto prose"
 	>
 		<Drawer position="top" bgDrawer="backdrop-blur bg-surface-backdrop-token" height="h-screen" duration={250}>
-			<div class="flex flex-col items-center px-2 pt-4">
+			<div class="flex flex-col items-center p-4">
 				<div class="flex items-center justify-between w-full">
-					<div class="px-5">
+					<div class="px-[11px]">
 						<LightSwitch />
 					</div>
 					<NavHamburgerMenu />
