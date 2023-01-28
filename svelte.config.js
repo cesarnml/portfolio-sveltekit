324ddfa151475f3bcc89d117527eab7c9e4b4c4d
Cesar Mejia <cmejia@gmail.com>
2023-01-28 09:58:02 +0700

    push up fix

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-vercel'
 import { mdsvex } from 'mdsvex'
 import { resolve } from 'path'
 import mdsvexConfig from './mdsvex.config.mjs'
-import preprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +11,7 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	// preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
-	preprocess: [preprocess({ postcss: true }), mdsvex(mdsvexConfig)],
+	preprocess: [vitePreprocess({ postcss: true }), mdsvex(mdsvexConfig)],
 
 	kit: {
 		adapter: adapter(),
