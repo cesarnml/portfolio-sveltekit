aecf883ef2270b737ca6593ad35714400c3eccf5
Cesar Mejia <cmejia@gmail.com>
2023-02-06 14:44:15 +0700

    minor

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -4,9 +4,6 @@
 	import NavMenu from './NavMenu.svelte'
 </script>
 
-<!-- <nav
-	class="sticky top-0 z-50 flex items-center justify-between h-16 px-4 shadow-md bg-slate-900/75 backdrop-blur sm:px-6"
-> -->
 <AppBar>
 	<svelte:fragment slot="lead"><NavBrand /></svelte:fragment>
 	<svelte:fragment slot="trail"><NavMenu /></svelte:fragment>
