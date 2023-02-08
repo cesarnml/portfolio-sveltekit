476a5f08ff4b9d1135ce199af0f7bd82e74c40c7
Cesar Mejia <cmejia@gmail.com>
2023-02-08 10:19:51 +0700

    switch codeToHtml for renderToHtml

--- src/lib/codeHighlighter.mjs
+++ src/lib/codeHighlighter.mjs
@@ -1,7 +1,13 @@
 import { parse } from 'node-html-parser'
-import { getHighlighter } from 'shiki'
+import { getHighlighter, renderToHtml } from 'shiki'
 import { readFile } from 'fs/promises'
-const synthwaveJson = JSON.parse(await readFile(new URL('./synthwave.json', import.meta.url)))
+const synthWave84Theme = JSON.parse(await readFile(new URL('./synthWave84.json', import.meta.url)))
+
+const htmlEscapes = {
+	'{': '&lbrace;',
+	'}': '&rbrace;',
+	'`': '&grave;',
+}
 
 /**
  * Returns code with curly braces and backticks replaced by HTML entity equivalents
@@ -9,7 +15,7 @@ const synthwaveJson = JSON.parse(await readFile(new URL('./synthwave.json', impo
  * @returns {string} - escaped HTML
  */
 function escapeHtml(code) {
-	return code.replace(/[{}`]/g, (character) => ({ '{': '&lbrace;', '}': '&rbrace;', '`': '&grave;' }[character]))
+	return code.replace(/[{}`]/g, (chr) => htmlEscapes[chr])
 }
 
 /**
@@ -37,72 +43,62 @@ function rangeParser(rangeString) {
 }
 
 /**
- * @param html {string} - code to highlight
- * @returns {string} - highlighted html
- */
-function makeFocusable(html) {
-	const root = parse(html)
-	root.querySelector('pre').setAttribute('tabIndex', '0')
-	return root.toString()
-}
-
-/**
  * @param code {string} - code to highlight
  * @param lang {string} - code language
  * @param meta {string} - code meta
  * @returns {string} - highlighted html
  */
 async function highlighter(code, lang, meta) {
-	const shikiHighlighter = await getHighlighter({
-		theme: synthwaveJson,
+	const { codeToThemedTokens, getForegroundColor, getBackgroundColor } = await getHighlighter({
+		theme: synthWave84Theme,
 	})
 
-	let html
-	if (!meta) {
-		html = shikiHighlighter.codeToHtml(code, {
-			lang,
-		})
-	} else {
-		// const highlightMeta = /{([\d,-]+)}/.exec(meta)[1]
-		const highlightMetaAdd = (meta.match(/[\d-]+a/g) ?? []).join(',').replaceAll('a', '')
-		const highlightMetaRemove = (meta.match(/[\d-]+r/g) ?? []).join(',').replaceAll('r', '')
-		const highlightMetaNormal = (meta.match(/[\d-]+n/g) ?? []).join(',').replaceAll('n', '')
-		const highlightMetaFocus = (meta.match(/[\d-]+f/g) ?? []).join(',').replaceAll('f', '')
+	const tokens = codeToThemedTokens(code, lang)
 
-		const highlightLinesAdd = rangeParser(highlightMetaAdd)
-		const highlightLinesRemove = rangeParser(highlightMetaRemove)
-		const highlightLinesNormal = rangeParser(highlightMetaNormal)
-		const highlightLinesFocus = rangeParser(highlightMetaFocus)
+	const metaAdd = (meta?.match(/[\d-]+a/g) ?? []).join(',').replaceAll('a', '')
+	const metaRemove = (meta?.match(/[\d-]+r/g) ?? []).join(',').replaceAll('r', '')
+	const metaNormal = (meta?.match(/[\d-]+n/g) ?? []).join(',').replaceAll('n', '')
+	const metaFocus = (meta?.match(/[\d-]+f/g) ?? []).join(',').replaceAll('f', '')
 
-		const isFocus = !!highlightLinesFocus.length
+	const highlightLinesAdd = rangeParser(metaAdd)
+	const highlightLinesRemove = rangeParser(metaRemove)
+	const highlightLinesNormal = rangeParser(metaNormal)
+	const highlightLinesFocus = rangeParser(metaFocus)
 
-		html = shikiHighlighter.codeToHtml(code, {
-			lang,
-			lineOptions: isFocus
-				? highlightLinesFocus.map((element) => ({ line: element, classes: ['highlight-line-focus'] }))
-				: [
-						...highlightLinesAdd.map((element) => {
-							return {
-								line: element,
-								classes: ['highlight-line-add'],
-							}
-						}),
-						...highlightLinesRemove.map((element) => {
-							return {
-								line: element,
-								classes: ['highlight-line-remove'],
-							}
-						}),
-						...highlightLinesNormal.map((element) => {
-							return {
-								line: element,
-								classes: ['highlight-line-normal'],
-							}
-						}),
-				  ],
-		})
-	}
-	html = makeFocusable(html)
+	const isFocus = !!highlightLinesFocus.length
+
+	const html = renderToHtml(tokens, {
+		fg: getForegroundColor(synthWave84Theme),
+		bg: getBackgroundColor(synthWave84Theme),
+		elements: {
+			pre({ className, style, children }) {
+				return `<pre tabindex="0" class="${className}" style="${style}">${children}</pre>`
+			},
+		},
+		lineOptions: isFocus
+			? highlightLinesFocus.map((element) => ({ line: element, classes: ['highlight-line-focus'] }))
+			: [
+					...highlightLinesAdd.map((element) => {
+						return {
+							line: element,
+							classes: ['highlight-line-add'],
+						}
+					}),
+					...highlightLinesRemove.map((element) => {
+						return {
+							line: element,
+							classes: ['highlight-line-remove'],
+						}
+					}),
+					...highlightLinesNormal.map((element) => {
+						return {
+							line: element,
+							classes: ['highlight-line-normal'],
+						}
+					}),
+			  ],
+	})
+
 	return escapeHtml(html)
 }
 
