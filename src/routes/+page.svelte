8a0ba75ff5de9a41fe9a1591cf21c566147523b8
Cesar Mejia <cmejia@gmail.com>
2023-02-12 10:15:41 +0700

    add lottie fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
 	import { page } from '$app/stores'
-	import Picture from '$lib/components/Picture.svelte'
 </script>
 
 <svelte:head>
 	<title>Cesar Mejia's Web Portfolio</title>
 	<link rel="canonical" href={$page.url.href} />
+	<script
+		async
+		src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"
+	></script>
 </svelte:head>
 
 <div class="flex h-full w-full flex-col items-center justify-center">
-	<div class="w-96">
-		<Picture
-			src="/src/lib/assets/picture/construction.png"
-			alt="construction"
-			loading="eager"
-			decoding="sync"
-		/>
-	</div>
-	<div>Site under construction.</div>
+	<lottie-player
+		src="https://assets7.lottiefiles.com/private_files/lf30_y9czxcb9.json"
+		background="transparent"
+		speed="1"
+		style="width: 500px; height: 500px;"
+		loop
+		autoplay
+	/>
+	<h2>Site under construction</h2>
 </div>
