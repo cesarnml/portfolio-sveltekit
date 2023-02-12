4b9aa4f791f03ed7ffec7bba143dfcbe87e177df
Cesar Mejia <cmejia@gmail.com>
2023-02-12 19:48:48 +0700

    change to lottie-web-light

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores'
 	import { onMount } from 'svelte'
-	import lottie from 'lottie-web'
+	import lottie from 'lottie-web-light'
 	import constructionAnimation from '$lib/assets/json/construction.json'
 
 	let lottieContainer: HTMLDivElement
