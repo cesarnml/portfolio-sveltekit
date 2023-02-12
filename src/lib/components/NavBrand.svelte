96865d97141ee14514a391f8ffeb075d515db164
Cesar Mejia <cmejia@gmail.com>
2023-02-12 19:41:54 +0700

    add brand

--- src/lib/components/NavBrand.svelte
+++ src/lib/components/NavBrand.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
 	import { Url } from '$lib/url'
 	import Picture from './Picture.svelte'
-	import SiteAuthorDetails from './SiteAuthorDetails.svelte'
 </script>
 
 <a class="flex items-center justify-center gap-2" href={Url.Home}>
-	<div class="h-12 w-12">
-		<Picture
-			class="h-12 w-full object-contain"
-			src="/src/lib/assets/picture/avatar.png"
-			alt="avatar"
-			loading="eager"
-			decoding="sync"
-		/>
+	<div class="aspect-[5/3] w-20">
+		<Picture src="/src/lib/assets/picture/brand.png" alt="brand" loading="eager" decoding="sync" />
 	</div>
-	<SiteAuthorDetails />
 </a>
