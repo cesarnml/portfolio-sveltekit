182a8f6634eaaa8fcaf9baba8c93214821e18f14
Cesar Mejia <cmejia@gmail.com>
2023-02-15 18:13:55 +0700

    fix ts warning

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,5 @@
 import { getSupabase } from '@supabase/auth-helpers-sveltekit'
-import { error } from '@sveltejs/kit'
-import type { LayoutLoad } from './$types'
+import { error, type LoadEvent } from '@sveltejs/kit'
 
 // Ensures all pages under this layout (which is all of them) are statically prerendered at build time
 export const prerender = true
@@ -8,7 +7,7 @@ export const prerender = true
 // Allows client side routing. Necessary for page transitions and link prefetching; change to false if you prefer ordinary routing without JS
 export const csr = true
 
-export const load: LayoutLoad = async (event) => {
+export const load = async (event: LoadEvent) => {
 	try {
 		const { session } = await getSupabase(event)
 		return {
