94664582e80fceb3035f101f3e16f6cb925c7ac8
Cesar Mejia <cmejia@gmail.com>
2023-02-23 13:49:09 +0700

    update navlink styling

--- src/lib/components/ReusableLink.svelte
+++ src/lib/components/ReusableLink.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores'
-	import { storeLightSwitch } from '@skeletonlabs/skeleton'
 
 	export let href = '/'
 	export let label = 'No label'
 
-	$: setActiveClass = (href: string) =>
-		href === $page.url.pathname ? 'text-secondary-400 dark:text-primary-400' : ''
+	$: setActiveClass = (href: string) => (href === $page.url.pathname ? 'text-primary-500' : '')
 </script>
 
-<a
-	{href}
-	class={`${setActiveClass(href)} hover:!bg-transparent ${
-		$storeLightSwitch ? 'hover:!text-primary-500' : 'hover:!text-secondary-800'
-	}`}>{label}</a
+<a {href} class={`${setActiveClass(href)} font-bold hover:!bg-transparent hover:!underline`}
+	>{label}</a
 >
