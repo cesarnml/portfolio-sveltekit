45e5c508b67e09f4291b21e93ce5468555d0212c
Cesar Mejia <cmejia@gmail.com>
2023-02-23 10:26:16 +0700

    config cssnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,8 +2,6 @@ module.exports = {
 	plugins: {
 		tailwindcss: {},
 		autoprefixer: {},
-		cssnano: {
-			preset: 'default',
-		},
+		...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
 	},
 }
