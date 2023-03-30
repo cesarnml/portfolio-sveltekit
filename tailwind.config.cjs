9db78cefaf367113b23b87881548131443173494
Cesar Mejia <cmejia@gmail.com>
2023-03-30 09:23:02 +0700

    done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -94,7 +94,6 @@ module.exports = {
 	plugins: [
 		require('@tailwindcss/forms'),
 		require('@tailwindcss/typography'),
-		require('@tailwindcss/line-clamp'),
 		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')(),
 	],
 }
