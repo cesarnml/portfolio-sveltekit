e14f49c48bfeb09c37e0f26b5a15cfda7b9bb125
Cesar Mejia <cmejia@gmail.com>
2023-04-07 02:36:45 +0700

    try again

--- src/lib/components/NavBrand.svelte
+++ src/lib/components/NavBrand.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { Url } from '$lib/url'
-	import Picture from './Picture.svelte'
 </script>
 
 <a class="flex items-center justify-center gap-2" href={Url.Home}>
 	<div class="aspect-[5/3] w-20">
-		<Picture src="/src/lib/assets/picture/brand.svg" alt="brand" loading="eager" decoding="sync" />
+		<img src="/src/lib/assets/picture/brand.svg" alt="brand" loading="eager" decoding="sync" />
 	</div>
 </a>
