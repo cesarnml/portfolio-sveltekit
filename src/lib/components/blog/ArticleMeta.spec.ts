8199e0492d5afa3135f7fd299edbd98919754e70
Cesar Mejia <cmejia@gmail.com>
2023-04-07 12:50:13 +0700

    fix svelte check lint error

--- src/lib/components/blog/ArticleMeta.spec.ts
+++ src/lib/components/blog/ArticleMeta.spec.ts
@@ -47,6 +47,7 @@ it("doesn't display cover image when `image` prop omitted", async () => {
 		viewCount: 1,
 	}
 
+	// @ts-expect-error Svelte issue
 	render(ArticleMeta, { props: articleWithoutImage })
 
 	// It does NOT display article cover image when `image` prop omitted
