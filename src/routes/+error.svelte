aba7f65a0e509e19ffa19df56911f4a76aed1125
Cesar Mejia <cmejia@gmail.com>
2023-04-10 13:49:58 +0700

    setup isr

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,18 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores'
-	import notFound from '$lib/assets/json/notFound.json'
 	import { Url } from '$lib/url'
-	import lottie from 'lottie-web-light'
-	import { onMount } from 'svelte'
-
-	let lottieContainer: HTMLDivElement
-
-	onMount(() => {
-		lottie.loadAnimation({
-			container: lottieContainer,
-			animationData: notFound,
-		})
-	})
 </script>
 
 <svelte:head>
@@ -22,9 +10,6 @@
 <div class="flex flex-col">
 	<h1 class="mb-4">Page Not Found</h1>
 	<div class="md:flex md:gap-4">
-		<div class="aspect-[1/1] w-1/3 md:w-1/4">
-			<div bind:this={lottieContainer} />
-		</div>
 		<div class="space-y-4 py-4">
 			{#if $page.status === 404}
 				<h2>We tried to find the page you requested:</h2>
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,3 @@
-export const prerender = true
-
 export const load = async ({ locals: { getSession } }) => ({
 	session: getSession(),
 })
