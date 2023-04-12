67b2e277dd61ebeea4a14eb2405e08be86e55128
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2023-04-12 14:28:36 +0700

    feat: Add blog post list implementation (#41)

--- src/lib/components/blog/BlogPostCard.svelte
+++ src/lib/components/blog/BlogPostCard.svelte
@@ -12,11 +12,23 @@
 </script>
 
 <a
-	class="blog-card relative flex w-full flex-col justify-between overflow-hidden"
+	class="blog-card card-hover relative flex w-full flex-col justify-between overflow-hidden"
 	href={Url.BlogDetail(post.slug)}
 	transition:fade
 >
-	<header><Picture class="object-cover" src={post.image} alt="post cover image" /></header>
+	<header class="relative">
+		<Picture class="object-cover" src={post.image} alt="post cover image" />
+		<div
+			class="badge badge-glass absolute right-2 top-2 border border-indigo-300 !bg-indigo-900/70 text-sm backdrop-blur-sm"
+		>
+			{post.readingTime.text}
+		</div>
+		<div
+			class="badge badge-glass absolute left-2 top-2 border border-zinc-300 !bg-zinc-900/70 text-sm backdrop-blur-sm"
+		>
+			{post.date}
+		</div>
+	</header>
 	<div class="flex h-full flex-col justify-between gap-4 p-4">
 		<h4 class="no-underline hover:underline">{post.title}</h4>
 		<div class="flex-grow font-normal">{post.description}</div>
@@ -34,8 +46,8 @@
 			<div class="text-right">
 				<span class=" text-sm font-normal"
 					>{view?.count ?? DEFAULT_VIEW_COUNT}
-					{`view${view?.count === 1 || view?.count === undefined ? '' : 's'}`}</span
-				>
+					{`view${view?.count === 1 || view?.count === undefined ? '' : 's'}`}
+				</span>
 			</div>
 		</footer>
 	</div>
