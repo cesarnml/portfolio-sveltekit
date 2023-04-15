ececa1e672b9ae167cf8af15ced597e8109e47a6
Cesar Mejia <cmejia@gmail.com>
2023-04-15 21:48:21 +0700

    fix layout

--- src/routes/api/shortcut/server.ts
+++ src/routes/api/shortcut/server.ts
@@ -1,8 +1,14 @@
 import { json, type RequestHandler } from '@sveltejs/kit'
 
 export const POST: RequestHandler = async ({ request }) => {
-	const payload = request.json()
+	const payload = await request.json()
 	console.log('payload:', payload)
 
+	return json({ message: 'ok' })
+}
+
+export const GET: RequestHandler = async ({ request }) => {
+	const payload = await request.json()
+	console.log('payload:', payload)
 	return json({ message: 'ok' })
 }
